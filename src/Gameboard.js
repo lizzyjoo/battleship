61f8225ec2d122b809bf62560645ac6c536819ba
lizzyjoo <jejviolinist@gmail.com>
2024-09-05 11:31:18 -0400

    cpu ship placement validity checking method

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -7,10 +7,9 @@ export class Gameboard {
       Array(size).fill(null)
     );
     this.ships = [];
+    this.tempGrid = Array.from({ length: size }, () => Array(size).fill(null));
   }
 
-  // function to place ship on the gameboard
-  predetermined(ship) {}
   placeShip(ship, x, y, isVertical) {
     if (isVertical) {
       if (x + ship.length > this.size) return false; // Out of bounds
@@ -18,7 +17,9 @@ export class Gameboard {
         if (this.grid[x + i][y] !== null) return false; // Collision with existing ship
       }
       for (let i = 0; i < ship.length; i++) {
-        this.grid[x + i][y] = ship; // Place the ship
+        if (ship.name !== "temp") {
+          this.grid[x + i][y] = ship; // Place the ship
+        }
       }
     } else {
       // horizontal placement
@@ -27,7 +28,31 @@ export class Gameboard {
         if (this.grid[x][y + i] !== null) return false; // Collision with existing ship
       }
       for (let i = 0; i < ship.length; i++) {
-        this.grid[x][y + i] = ship; // Place the ship
+        if (ship.name !== "temp") {
+          this.grid[x][y + i] = ship; // Place the ship
+        }
+      }
+    }
+    return true; // Successfully placed the ship
+  }
+
+  checkCPUShipPlacement(ship, x, y, isVertical) {
+    if (isVertical) {
+      if (x + ship.length > this.size) return false; // Out of bounds
+      for (let i = 0; i < ship.length; i++) {
+        if (this.tempGrid[x + i][y] !== null) return false; // Collision with existing ship
+      }
+      for (let i = 0; i < ship.length; i++) {
+        this.tempGrid[x + i][y] = ship; // Place the ship
+      }
+    } else {
+      // horizontal placement
+      if (y + ship.length > this.size) return false; // Out of bounds
+      for (let i = 0; i < ship.length; i++) {
+        if (this.tempGrid[x][y + i] !== null) return false; // Collision with existing ship
+      }
+      for (let i = 0; i < ship.length; i++) {
+        this.tempGrid[x][y + i] = ship; // Place the ship
       }
     }
     return true; // Successfully placed the ship
